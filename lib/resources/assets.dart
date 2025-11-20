bd895246f66d9db72adaf93bffe091dd7ea29099
Asumadu Frank Afriyie <afriyiefrankasumadu@gmail.com>
2025-11-20 11:11:41 +0000

    first commit

---
+++ lib/resources/assets.dart
@@ -0,0 +1,12 @@
+part of 'resources.dart';
+
+class Assets {
+  Assets._();
+
+  static const String h1 = 'assets/h1.png';
+  static const String h2 = 'assets/h2.png';
+  static const String h3 = 'assets/h3.png';
+  static const String i1 = 'assets/i1.png';
+  static const String i2 = 'assets/i2.png';
+  static const String i3 = 'assets/i3.png';
+}
---
+++ test/assets_test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nft/resources/resources.dart';
+
+void main() {
+  test('assets assets test', () {
+    expect(File(Assets.h1).existsSync(), isTrue);
+    expect(File(Assets.h2).existsSync(), isTrue);
+    expect(File(Assets.h3).existsSync(), isTrue);
+    expect(File(Assets.i1).existsSync(), isTrue);
+    expect(File(Assets.i2).existsSync(), isTrue);
+    expect(File(Assets.i3).existsSync(), isTrue);
+  });
+}
