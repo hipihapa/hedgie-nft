746078fe634d3598460c72f34d098f50c37ba73b
Asumadu Frank Afriyie <afriyiefrankasumadu@gmail.com>
2025-11-20 11:56:30 +0000

    Update dependencies, add iOS and macOS support files, and enhance the intro screen layout. Updated package versions in pubspec.lock and pubspec.yaml, added Podfile for iOS, and implemented Google Fonts in the intro screen for improved typography.

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nft/resources/resources.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
@@ -6,10 +8,79 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Intro Screen'),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 30.0,
+                    bottom: 6.0,
+                    left: 10.0,
+                    right: 10.0,
+                  ),
+                  child: Container(
+                    width: 280,
+                    height: 340,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(40),
+                      image: DecorationImage(
+                        image: AssetImage(Assets.i1),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  "Find, Collect and Sell",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Amazing NFTs",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                Text(
+                  textAlign: TextAlign.center,
+                  "Explore the top collection of NFT's and",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  "buy and sell your NFTs as well",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
